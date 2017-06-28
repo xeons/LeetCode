233dbbf846cb87db8e21aca8fc4f93f13ba6259c
Brandon Scott <xeon.productions@gmail.com>
2017-06-27 21:04:37 -0500

    Initial commit.

---
+++ P004-Median-Two-Sorted-Arrays/P004-Median-Two-Sorted-Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P004_Median_Two_Sorted_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.FindMedianSortedArrays(new int[] { 1, 3 }, new int[] { 2 })); // 2.0
+            Console.WriteLine(solution.FindMedianSortedArrays(new int[] { 1, 2 }, new int[] { 3, 4 })); // 2.5
+            Console.WriteLine(solution.FindMedianSortedArrays(new int[] { 1, 2, 3 }, new int[] { 4, 5, 6 })); // 3.5
+            Console.WriteLine(solution.FindMedianSortedArrays(new int[] { 1 }, new int[] { 2 })); // 1.5
+            Console.WriteLine(solution.FindMedianSortedArrays(new int[] { }, new int[] { 1 })); // 1
+            Console.WriteLine(solution.FindMedianSortedArrays(new int[] { 1, 1 }, new int[] { 1, 2 })); // 1
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P004-Median-Two-Sorted-Arrays/P004-Median-Two-Sorted-Arrays/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P004_Median_Two_Sorted_Arrays
+{
+    class Solution
+    {
+        /**
+         * 2080 / 2080 test cases passed.
+	     * Status: Accepted
+	     * Runtime: 239 ms
+         */
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            int[] combined = nums1.Concat(nums2).OrderBy(i => i).ToArray();
+            return combined.Length % 2 == 0 ? (double)(combined[(combined.Length / 2) - 1] + combined[(combined.Length / 2)]) / 2 : combined[(int)combined.Length / 2];
+        }
+    }
+}
