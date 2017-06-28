f525db50fb52043f7ff154bb29b7b41766ad5a1e
Brandon Scott <xeon.productions@gmail.com>
2017-06-27 19:34:05 -0500

    Initial commit.

---
+++ P001-Two-Sum/P001-Two-Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P001_Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] result = solution.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            Console.WriteLine("Result: [{0}], Expected: [{1}]", string.Join(", ", result), "0, 1");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P001-Two-Sum/P001-Two-Sum/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P001_Two_Sum
+{
+    public class Solution
+    {
+        /*
+         * 19 / 19 test cases passed.
+         * Status: Accepted.
+         * Runtime: 715 ms
+         */
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                for (int j = 0; j < nums.Length; j++)
+                    if (j != i && nums[i] + nums[j] == target)
+                        return new int[] { i, j };
+            return null;
+        }
+    }
+}
