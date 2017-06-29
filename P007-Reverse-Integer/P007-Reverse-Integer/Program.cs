711ec9c53185df2092ded3830a2806c480370c39
Brandon Scott <xeon.productions@gmail.com>
2017-06-28 20:06:08 -0500

    Initial commit.

---
+++ P007-Reverse-Integer/P007-Reverse-Integer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P007_Reverse_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+
+            Console.WriteLine(solution.Reverse(-123));              // -321
+            Console.WriteLine(solution.Reverse(-456));              // -654
+            Console.WriteLine(solution.Reverse(123456));            // 654321
+            Console.WriteLine(solution.Reverse(-2147483648));       // 0
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P007-Reverse-Integer/P007-Reverse-Integer/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P007_Reverse_Integer
+{
+    class Solution
+    {
+        /*
+         * 1032 / 1032 test cases passed.
+         * Status: Accepted
+         * Runtime: 82 ms
+         */
+        public int Reverse(int x)
+        {
+            long temp = Math.Abs((long)x), carry = 0;
+            long output = 0;
+            int digits = (int)Math.Floor(Math.Log10(temp) + 1);
+
+            while (temp > 0)
+            {
+                carry = temp % 10;
+                temp = temp / 10;
+                output = output + (long)Math.Pow(10, --digits) * carry;
+            }
+
+            return x > 0 ? (output > int.MaxValue ? 0 : (int)output) : (-output < int.MinValue ? 0 : (int)-output);
+        }
+    }
+}
