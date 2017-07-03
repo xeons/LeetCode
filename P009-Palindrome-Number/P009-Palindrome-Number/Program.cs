7b80a07525f4b7b5a3b2a4c806d550b200737169
Brandon Scott <xeon.productions@gmail.com>
2017-07-03 16:19:37 -0500

    Initial commit

---
+++ P009-Palindrome-Number/P009-Palindrome-Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P009_Palindrome_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = new Solution();
+            Console.WriteLine(solution.IsPalindrome(123));
+            Console.WriteLine(solution.IsPalindrome(123321));
+            Console.WriteLine(solution.IsPalindrome(int.MaxValue));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P009-Palindrome-Number/P009-Palindrome-Number/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P009_Palindrome_Number
+{
+    class Solution
+    {
+        /**
+         * 11507 / 11507 test cases passed.
+         * Status: Accepted
+         * Runtime: 122 ms
+         */
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+
+            int y = x, reversed = 0;
+            while (x > 0)
+            {
+                reversed = reversed * 10 + x % 10;
+                x = x / 10;
+            }
+
+            return y == reversed;
+        }
+    }
+}
